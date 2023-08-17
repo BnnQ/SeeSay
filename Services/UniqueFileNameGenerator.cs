bd8e26674e06bf185061181e6dae1ef98b2d1530
BnnQ <1v25fa94@gmail.com>
2023-08-17 13:56:54 +0300

    Added UniqueFileNameGenerator service (implementation of IFileNameGenerator)

---
+++ Services/UniqueFileNameGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using SeeSay.Services.Abstractions;
+
+namespace SeeSay.Services;
+
+public class UniqueFileNameGenerator : IFileNameGenerator
+{
+    public string GenerateFileName(string? baseFileNameWithoutExtension = null, string? fileNameExtension = null)
+    {
+        StringBuilder fileNameBuilder = new();
+        if (!string.IsNullOrWhiteSpace(baseFileNameWithoutExtension))
+        {
+            fileNameBuilder.Append(baseFileNameWithoutExtension)
+                .Append('_');
+        }
+        fileNameBuilder.Append(Guid.NewGuid().ToString("N"));
+            
+        var fileName = fileNameBuilder.ToString();
+        if (!string.IsNullOrWhiteSpace(fileNameExtension))
+        {
+            fileName = Path.ChangeExtension(fileName, fileNameExtension);
+        }
+
+        return fileName;
+    }
+}
